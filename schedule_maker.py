2e0d89fb1d70cc6b236c05861ca5308b9887bb22
Rohil-Dave <rohil.dave20@imperial.ac.uk>
2024-11-01 22:31:51 +0000

    Add generate_random_standings function to create randomized division standings

--- schedule_maker.py
+++ schedule_maker.py
@@ -1,3 +1,17 @@
 '''
 This project is about creating alternative NFL schedules based on an optimized variable of choice.
 '''
+
+import random
+from data import teams, conferences
+
+def generate_random_standings():
+    """Randomize standings within each division and return a dictionary."""
+    standings = {}
+    for conference, divisions in conferences.items():
+        standings[conference] = {}
+        for division, division_teams in divisions.items():
+            # Shuffle teams to create random standings within the division
+            random_standings = random.sample(division_teams, len(division_teams))
+            standings[conference][division] = random_standings
+    return standings
